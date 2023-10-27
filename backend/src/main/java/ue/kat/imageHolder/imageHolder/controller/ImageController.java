75c631c2328c1143bb45b7c40b26d03f3ed8987e
Pawel Mika <pawel.mika@softiq.pl>
2023-10-27 19:29:04 +0200

    feature: Flyway config

--- backend/src/main/java/ue/kat/imageHolder/imageHolder/controller/ImageController.java
+++ backend/src/main/java/ue/kat/imageHolder/imageHolder/controller/ImageController.java
@@ -1,6 +1,5 @@
 package ue.kat.imageHolder.imageHolder.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
